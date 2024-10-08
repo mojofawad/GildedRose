b0c40162d49bb976b468eab65e7f27b49a07216a
Joey Fawad <mojofawad@gmail.com>
2024-10-08 00:42:03 -0400

    fix QualityChangeModifier issue causing missing code coverage

--- src/GildedRose.Library/InventoryItems/Interfaces/ValuableItemBase.cs
+++ src/GildedRose.Library/InventoryItems/Interfaces/ValuableItemBase.cs
@@ -2,17 +2,10 @@
 {
     public abstract class ValuableItemBase : NonLegendaryItem
     {
-        private const int MaximumItemQuality = 50;
-
         protected ValuableItemBase(Item item) : base(item)
         {
         }
 
         protected override int QualityChangeModifier => 1;
-
-        private bool ItemQualityLessThanMaximum()
-        {
-            return _item.Quality < MaximumItemQuality;
-        }
     }
 }
